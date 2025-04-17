e466c95b3230b763b0a616a077a3f1df1d8f8ac5
Junsung Choi <choiexe1@gmail.com>
2025-04-17 18:46:12 +0900

    feat: load env

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:modu_image_search_app/core/router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:modu_image_search_app/core/routing/router.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
