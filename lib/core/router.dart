69f859290fd4827fb936d36e668ece281ef478f6
Junsung Choi <choiexe1@gmail.com>
2025-04-17 16:36:01 +0900

    feat: router

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -5,6 +5,6 @@ import 'package:modu_image_search_app/presentation/home_screen.dart';
 final GoRouter router = GoRouter(
   initialLocation: Routes.home,
   routes: [
-    GoRoute(path: Routes.home, builder: (context, state) => const HomeScreen()),
+    GoRoute(path: Routes.home, builder: (context, state) => HomeScreen()),
   ],
 );
