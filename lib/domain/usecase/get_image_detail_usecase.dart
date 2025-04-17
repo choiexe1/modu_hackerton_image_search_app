16454368a76c80ada6fa45c963465102ef396102
Junsung Choi <choiexe1@gmail.com>
2025-04-17 18:45:34 +0900

    feat: 다중 이미지 로드 유즈케이스, 단일 이미지 로드 유즈케이스

--- lib/domain/usecase/get_image_detail_usecase.dart
+++ lib/domain/usecase/get_image_detail_usecase.dart
@@ -1,12 +1,12 @@
 import 'package:modu_image_search_app/domain/model/custom_image.dart';
 import 'package:modu_image_search_app/domain/repository/image_repository.dart';
 
-class GetImageUseCase {
+class GetImageDetailUsecase {
   final ImageRepository _imageRepository;
 
-  const GetImageUseCase(this._imageRepository);
+  const GetImageDetailUsecase(this._imageRepository);
 
-  Future<List<CustomImage>> execute(String query) async {
-    return await _imageRepository.searchImage(query);
+  Future<CustomImage> execute(int id) async {
+    return await _imageRepository.getImageDetail(id);
   }
 }
