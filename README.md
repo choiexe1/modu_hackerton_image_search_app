1a1217238728e2ea4ddf1114163401a13faf2ee1
Junsung Choi <choiexe1@gmail.com>
2025-04-17 15:16:05 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# modu_image_search_app
+
+- 모두의 연구소 플러터 과정 해커톤
---
+++ android/app/src/main/kotlin/com/example/modu_image_search_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.modu_image_search_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
