69e5be7febba361a28959468073cff787055bac5
Junsung Choi <choiexe1@gmail.com>
2025-04-17 15:31:37 +0900

    feat: debug banner remove

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:modu_image_search_app/core/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +9,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(title: 'Image Search App');
+    return MaterialApp.router(
+      title: 'Image Search App',
+      routerConfig: router,
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
